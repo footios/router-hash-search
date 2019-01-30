70c7c1e0937551e65d1b9e0b06d585a72c0d5ca7
footios <footios76@gmail.com>
2019-01-30 16:05:10 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is from MLR in Q&A or this [Udemy course](https://www.udemy.com/react-the-complete-guide-incl-redux/)
 
 [Purpose of Fragment/hash and Query/search](https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/questions/3469586)
+
+index.js has the refactored code from MLR again.
--- src/index.js
+++ src/index.js
@@ -1,129 +1,105 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+//=========================================================================
+const myExternalProcessor = myProps => {
+  const myHash = myProps.location.hash;
+  let mySearchValueArray = [];
+  const mySearches = new URLSearchParams(myProps.location.search);
+  console.log("myProps.location.search: ", myProps.location.search);
+  // 'myProps.location.search'
+  // returns this: ?my-search-writtenColor=green-search&my-search-bgdColor=%235e9
+  // which is from Link -> search (below):
+  // `?my-search-writtenColor=` +
+  // `${myCurrentItem.writtenColor}-search` +
+  // `&my-search-bgdColor=` +
+  // `${encodeURIComponent(myCurrentItem.bgdColor)}`
 
-// With function you get and return the 'hash' and the 'search'.
-// The color components use it to print them (hash/search).
-const myExternalProcessor = props => {
-  console.log("props: ", props);
-  // Further down below, in each Link
-  // you give a value for the 'hash' and the 'search'
-  // of the 'location'
-  // Here you access them through props
-  const myHash = props.location.hash;
-  console.log("myHash: ", myHash); // myHash:  #pink-hash
-  let mySearchValue = "";
-  // The URLSearchParams interface defines utility methods
-  // to work with the query string of a URL.
-  const mySearch = new URLSearchParams(props.location.search);
-  //  const mySearch = "?my-search=pink-color"
-  // The entries() method of the URLSearchParams interface returns
-  // an iterator allowing iteration through all key/value pairs
-  // contained in this object.
-  for (let myParam of mySearch.entries()) {
-    console.log("myParam: ", myParam); // ["my-search", "pink-color"]
-    mySearchValue = myParam[1];
-  }
-  // So here myExternalProcessor return an array:
-  // e.g ["#pink-hash", "[pink]-color"]
-  return [myHash, mySearchValue];
+  console.log("mySearches: ", mySearches); // logs utility methods
+
+  mySearches.forEach(currentParam => {
+    mySearchValueArray.push(currentParam);
+  });
+  console.log("mySearchValueArray: ", mySearchValueArray); //  ["pink-search", "#fac"]
+
+  return [myHash, mySearchValueArray];
 };
-const MyComponentPink = props => {
-  const myPinkBgd = { padding: "6px 0", backgroundColor: "#fac" };
+//=========================================================================
+const MyComponent = props => {
+  const myStyle = {
+    padding: "6px 0",
+    backgroundColor: myExternalProcessor(props)[1][1] // #fac
+  };
+  console.log("myExternalProcessor(props): ", myExternalProcessor(props));
+  // "#pink-hash"
+  // ["pink-search", "#fac"]
+
   return (
-    <div style={myPinkBgd}>
-      {console.log("myExternalProcessor: ", myExternalProcessor(props)[0])}
-      {myExternalProcessor(props)[0]}
-      {/* #pink-hash */}
+    <div style={myStyle}>
+      {myExternalProcessor(props)[0]} {/*#pink-hash*/}
       {" & "}
-      {myExternalProcessor(props)[1]}
-      {/* #pink-color */}
-    </div>
-    // So every component returns its hash and color.
-  );
-};
-const MyComponentGreen = props => {
-  const myPinkBgd = { padding: "6px 0", backgroundColor: "#5e9" };
-  return (
-    <div style={myPinkBgd}>
-      {myExternalProcessor(props)[0]}
-      {" & "}
-      {myExternalProcessor(props)[1]}
+      {myExternalProcessor(props)[1][0]} {/*pink-search*/}
     </div>
   );
 };
-const MyComponentBlue = props => {
-  const myPinkBgd = { padding: "6px 0", backgroundColor: "#9bf" };
-  return (
-    <div style={myPinkBgd}>
-      {myExternalProcessor(props)[0]}
-      {" & "}
-      {myExternalProcessor(props)[1]}
-    </div>
-  );
-};
-class App extends Component {
+//=========================================================================
+class MyApp extends Component {
+  state = {
+    myColorsArray: [
+      { writtenColor: "pink", bgdColor: "#fac" },
+      { writtenColor: "green", bgdColor: "#5e9" },
+      { writtenColor: "blue", bgdColor: "#9bf" },
+      { writtenColor: "vvv", bgdColor: "#9bf" }
+    ]
+  };
+  //-------------------------------------------------------------------------
+  myContainer = {
+    maxWidth: "360px",
+    margin: "10px auto",
+    padding: "10px 0",
+    backgroundColor: "#ddd",
+    textAlign: "center",
+    border: "1px solid #000",
+    fontSize: "16px",
+    fontFamily: "Helvetica"
+  };
+  myUlStyle = {
+    width: "100%",
+    margin: "0",
+    padding: "0",
+    listStyle: "none",
+    textAlign: "center"
+  };
+  myLiStyle = { display: "inline-block", margin: "0 4px 10px" };
+  //-------------------------------------------------------------------------
+  myMapLinkMethod = () =>
+    this.state.myColorsArray.map(myCurrentItem => (
+      <li key={myCurrentItem.writtenColor} style={this.myLiStyle}>
+        <Link
+          to={{
+            pathname: `/${myCurrentItem.writtenColor}`,
+            hash: `#${myCurrentItem.writtenColor}-hash`,
+            search:
+              `?my-search-writtenColor=` +
+              `${myCurrentItem.writtenColor}-search` +
+              `&my-search-bgdColor=` +
+              `${encodeURIComponent(myCurrentItem.bgdColor)}`
+          }}
+        >
+          {myCurrentItem.writtenColor}
+        </Link>
+      </li>
+    ));
+  //-------------------------------------------------------------------------
   render() {
-    const myContainer = {
-      maxWidth: "360px",
-      margin: "10px auto",
-      padding: "10px 0",
-      backgroundColor: "#ddd",
-      textAlign: "center",
-      border: "1px solid #000",
-      fontSize: "16px",
-      fontFamily: "Helvetica",
-      fontWeight: "lighter"
-    };
-    const ulStyle = {
-      width: "100%",
-      margin: "0",
-      padding: "0",
-      listStyle: "none",
-      textAlign: "center"
-    };
-    const liStyle = { display: "inline-block", margin: "0 4px 10px" };
     return (
-      <div style={myContainer}>
-        <ul style={ulStyle}>
-          <li style={liStyle}>
-            <Link
-              to={{
-                pathname: "/pink",
-                hash: "#pink-hash",
-                search: "?my-search=pink-color"
-              }}
-            >
-              Pink
-            </Link>
-          </li>
-          <li style={liStyle}>
-            <Link
-              to={{
-                pathname: "/green",
-                hash: "#green-hash",
-                search: "?my-search=green-color"
-              }}
-            >
-              Green
-            </Link>
-          </li>
-          <li style={liStyle}>
-            <Link
-              to={{
-                pathname: "/blue",
-                hash: "#blue-hash",
-                search: "?my-search=blue-color"
-              }}
-            >
-              Blue
-            </Link>
-          </li>
-        </ul>
+      <div style={this.myContainer}>
+        <ul style={this.myUlStyle}>{this.myMapLinkMethod()}</ul>
         <Switch>
-          <Route path="/pink" component={MyComponentPink} />
-          <Route path="/green" component={MyComponentGreen} />
-          <Route path="/blue" component={MyComponentBlue} />
+          <Route path="/pink" component={MyComponent} />
+          <Route path="/green" component={MyComponent} />
+          <Route path="/blue" component={MyComponent} />
+          <Route path="/vvv" component={MyComponent} />
         </Switch>
       </div>
     );
@@ -131,7 +107,7 @@ class App extends Component {
 }
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <MyApp />
   </BrowserRouter>,
   document.getElementById("root")
 );
